e87e36684ea066f767c8d89f4a02b5e6d7fbc427
Eddie Murigu <ewamais@yahoo.com>
2019-08-21 15:42:49 +0300

    added reset feature

---
+++ app.js
@@ -0,0 +1,68 @@
+const clearBtn = document.querySelector(".fa-refresh");
+
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
+
+function calculateResults() {
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
+  } else {
+    showError("check your numbers");
+  }
+}
+
+function showError(error) {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "none";
+  const errorDiv = document.createElement("div");
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  errorDiv.className = "alert alert-danger";
+  errorDiv.appendChild(document.createTextNode(error));
+  card.insertBefore(errorDiv, heading);
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector(".alert").remove();
+}
+
+// Added by me on 20th Aug 2019
+clearBtn.addEventListener("click", clearFields);
+
+function clearFields() {
+  location.reload();
+}
+
+// Code from Rajju. What i was trying to do
+document.addEventListener("click", function(e) {
+  if (e.target.id === "clear") {
+    document.querySelector("form").style.display = "block";
+    document.querySelector("#results").style.display = "none";
+    document.getElementById("amount").value = "";
+    document.getElementById("interest").value = "";
+    document.getElementById("years").value = "";
+  }
+});
